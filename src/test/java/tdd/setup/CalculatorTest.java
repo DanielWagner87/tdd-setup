7460cc3dda376ee2f8e9947e09207592ccba6f72
DanielWagner87 <daniel.wagner@europace.de>
2020-10-27 08:50:19 +0100

    first red test

--- src/test/java/tdd/setup/CalculatorTest.java
+++ src/test/java/tdd/setup/CalculatorTest.java
@@ -29,4 +29,20 @@ class CalculatorTest {
         calc.pressClearKey();
         assertEquals("0", calc.readScreen());
     }
+
+    @Test
+    @DisplayName("should add two decimal numbers")
+    void addDecimalNumbers() {
+        Calculator calc = new Calculator();
+        calc.pressDigitKey(2);
+        calc.pressDotKey();
+        calc.pressDigitKey(5);
+        calc.pressOperationKey("+");
+        calc.pressDigitKey(2);
+        calc.pressDotKey();
+        calc.pressDigitKey(6);
+        calc.pressEquals();
+        assertEquals("5.1", calc.readScreen());
+    }
+
 }
